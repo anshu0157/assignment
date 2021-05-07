5bfbf3a6eb6eea3f9f22507e14333f6ae920809f
Alpha9(The Falcon) <38017870+anshu0157@users.noreply.github.com>
2021-05-07 21:49:53 +0530

    Update problem_statement.py

--- assignment/problem_statement.py
+++ assignment/problem_statement.py
@@ -6,17 +6,16 @@ class student:
     def getStudentsWithMarksMoreThan(self):
         with open('data.json') as json_file:
             dict = json.load(json_file)
-        print(dict)
-        print('\n')
         dict1={}
         l=[]
         for key,value in dict.items():
-            # print(key,value)
             for x in range(len(value)):
                 details=value[x].get('details')
                 if details.get('marks')>self.marks:
                     l.append(value[x])
                     dict1.update({key:l})
+       
+        # converts it into json   
         jsd=json.dumps(dict)
         print(jsd)
                     
